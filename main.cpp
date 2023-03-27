b0922a2bd67a4f401c65c967ebea47015749248c
MorisZheng <s04044mort@gmail.com>
2023-03-27 17:15:41 +0800

    update

--- main.cpp
+++ main.cpp
@@ -16,8 +16,7 @@ Thread sensorThread;
 Thread queueThread;
 EventQueue queue(32 * EVENTS_EVENT_SIZE);
 
-float sample[100];
-float sensorValue;
+float sample[20];
 int sample_cnt = 0;
 
 
@@ -28,31 +27,39 @@ void pwmLED()
     {
         LED.pulsewidth_us(5000 * sample[cnt]);
         cnt++;
-        if (cnt == 100) cnt = 0;
-        ThisThread::sleep_for(10ms);
+        if (cnt == 20) cnt = 0;
+        ThisThread::sleep_for(50ms);
     }
 }
 
 void sensor()
 {
+    float sensorValue[50];
+    float averageValue;
+
     while (true){
-        sensorValue = lightSensor.read();
-        aout = sensorValue;
-        if (sensorValue > 0.5)
+        averageValue = 0;
+        if (sample_cnt > 5000)
+            sample_cnt = 0;
+        for (int i = 0; i < 50; i++){
+            sensorValue[i] = lightSensor.read();
+            sample_cnt++;
+            //aout = sensorValue[i];
+            averageValue += sensorValue[i];
+            ThisThread::sleep_for(1ms);
+        }
+        averageValue /= 50;
+        if (averageValue > 0.4)
             pinA = 1;
         else
             pinA = 0;
         led1 = pinA;
-        sample_cnt++;
-        if (sample_cnt > 100000)
-            sample_cnt = 0;
-        ThisThread::sleep_for(1ms);
     }
 }
 
-void printText (int state)
+void printText (int state, int cnt)
 {
-    uLCD.locate(3,2);
+    uLCD.locate(2,2);
     if (state == RISING){
         uLCD.color(RED);
         uLCD.printf("RISE");
@@ -62,39 +69,42 @@ void printText (int state)
         uLCD.printf("FALL");
     }
     uLCD.color(WHITE);
-    uLCD.locate(3,4);
-    uLCD.printf("%5d", sample_cnt);
+    uLCD.locate(2,4);
+    uLCD.printf("%4d", cnt);
+    printf("%3d\r\n", cnt);
 }
 
 void riseISR ()
 {
-    queue.call(printText, RISING);
+    queue.call(printText, RISING, sample_cnt);
 }
 
 void fallISR ()
 {
-    queue.call(printText, FALLING);
+    queue.call(printText, FALLING, sample_cnt);
 }
 
 int main()
 {
-    for (int i = 0; i < 100; i++){
-        if (i < 25)
-            sample[i] = (float)i / 25;
-        else if (i < 50)
+    for (int i = 0; i < 20; i++){
+        if (i < 5)
+            sample[i] = (float)i / 5;
+        else if (i < 10)
             sample[i] = 1.;
-        else if (i < 75)
-            sample[i] = 1. - (i - 50.) / 25;
+        else if (i < 15)
+            sample[i] = 1. - (i - 10.) / 5;
         else
             sample[i] = 0;
     }
     LED.period_ms(5);
     uLCD.text_width(2);
     uLCD.text_height(2);
-    sensorThread.start(sensor);
     LEDThread.start(pwmLED);
+    sensorThread.start(sensor);
     queueThread.start(callback(&queue, &EventQueue::dispatch_forever));
     pinB.rise(&riseISR);
     pinB.fall(&fallISR);
-    while (true) ;
+    while (true){
+        aout = pinB;
+    }
 }
